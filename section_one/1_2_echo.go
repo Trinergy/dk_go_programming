33cc353976ea9228af0ea4e9220ba37aeb36704d
Kenny Wu <wu.kenny1221@gmail.com>
2018-04-29 21:37:14 -0700

    Section 1 - exercises .1 and .2

--- section_one/1_2_echo.go
+++ section_one/1_2_echo.go
@@ -1,1 +1,46 @@
-placeholder
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// default
+func simpleEcho() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+// 1.1
+func argZeroEcho() {
+	s, sep := "", ""
+	for _, arg := range os.Args[:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+// 1.2
+func echoWithIndex() {
+	for i, arg := range os.Args[1:] {
+		fmt.Println(i, arg)
+	}
+}
+
+// 1.3
+func echoWithJoin() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func main() {
+	// simpleEcho()
+	// argZeroEcho()
+	// echoWithIndex()
+	// echoWithJoin()
+}
