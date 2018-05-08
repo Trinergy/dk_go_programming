8c1c78d3a970e56d90b6727582154e3a5a56f38b
Kenny Wu <wu.kenny1221@gmail.com>
2018-05-07 22:53:47 -0700

    Rename fetch to reflect exercise numbers + finish exercises

--- section_one/fetch/1_7-1_9_fetch.go
+++ section_one/fetch/1_7-1_9_fetch.go
@@ -3,24 +3,34 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const prefix string = "http://"
 
 func main() {
 	for _, url := range os.Args[1:] {
+		// 1.8 Add http:// prefix if not included in url
+		if !strings.HasPrefix(prefix, url) {
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		// 1.7 Use io.Copy instead of ioutil.ReadAll
+		b, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+
+		// 1.9 Return status code from request
+		fmt.Printf("Bytes Copied: %v STATUS CODE: %v", b, resp.StatusCode)
 	}
 }
